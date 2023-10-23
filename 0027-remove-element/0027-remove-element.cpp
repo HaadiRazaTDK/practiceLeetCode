7a9055bcb932e430a941fa9af8ee15bac796fd6b
Syed haadi Raza <129632623+HaadiRazaTDK@users.noreply.github.com>
2023-10-23 23:43:33 +0500

    Time: 0 ms (100%), Space: 9 MB (46.43%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+        return nums.size();
+    }
+};
+
+
+
